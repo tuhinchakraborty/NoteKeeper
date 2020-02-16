d04a6dd1e73c60bbdc7881fc1e8df6d9f5ded037
tuhinchakraborty <tuhin_chakraborty@outlook.com>
2020-02-16 22:13:11 +0530

    Add automation tests to navigate notes and view notes

--- app/src/androidTest/java/com/example/notekeeper/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/notekeeper/MainActivityTest.kt
@@ -7,7 +7,11 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.example.notekeeper.activities.NoteListActivity
+import com.example.notekeeper.models.Note
+import com.example.notekeeper.services.DataManager
+import junit.framework.Assert.assertEquals
 import org.hamcrest.Matchers
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,19 +23,99 @@ class MainActivityTest {
     @Rule // tells the test system this rule exists
     val noteListActivity = ActivityTestRule(NoteListActivity::class.java) // starts the activity
 
+    var testNotes: List<Note> = emptyList()
+
+    @Before
+    fun setUp() {
+        testNotes = DataManager.notes
+    }
+
     @Test
-    fun createNewNote() {
+    fun shouldCreateNote() {
         val title = "title"
         val text = "text"
 
         Espresso.onView(ViewMatchers.withId(R.id.fab)).perform(ViewActions.click())
 
         val anyMatch = Matchers.anything()
-        Espresso.onView(ViewMatchers.withId(R.id.noteBodyText)).check(ViewAssertions.matches(anyMatch))
-        Espresso.onView(ViewMatchers.withId(R.id.noteTitleText)).check(ViewAssertions.matches(anyMatch))
+        Espresso.onView(ViewMatchers.withId(R.id.noteBodyText))
+            .check(ViewAssertions.matches(anyMatch))
+        Espresso.onView(ViewMatchers.withId(R.id.noteTitleText))
+            .check(ViewAssertions.matches(anyMatch))
+
         Espresso.onView(ViewMatchers.withId(R.id.fab)).check(ViewAssertions.doesNotExist())
 
-        Espresso.onView(ViewMatchers.withId(R.id.noteTitleText)).perform(ViewActions.typeText(title))
-        Espresso.onView(ViewMatchers.withId(R.id.noteBodyText)).perform(ViewActions.typeText(text))
+        Espresso.onView(ViewMatchers.withId(R.id.noteTitleText))
+            .perform(ViewActions.typeText(title))
+        Espresso.onView(ViewMatchers.withId(R.id.noteBodyText))
+            .perform(ViewActions.typeText(text), ViewActions.closeSoftKeyboard())
+        Espresso.pressBack()
+
+        val note = testNotes.last()
+        assertEquals(title, note.title)
+        assertEquals(text, note.text)
+    }
+
+    @Test
+    fun shouldViewNote() {
+        val note = testNotes[0]
+        Espresso.onData(
+            Matchers.allOf(
+                Matchers.instanceOf(Note::class.java),
+                Matchers.equalTo(note)
+            )
+        ).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.noteTitleText))
+            .check(ViewAssertions.matches(ViewMatchers.withText(note.title)))
+        Espresso.onView(ViewMatchers.withId(R.id.noteBodyText))
+            .check(ViewAssertions.matches(ViewMatchers.withText(note.text)))
+    }
+
+    @Test
+    fun shouldNavigateToNextNote() {
+        Espresso.onData(
+            Matchers.allOf(
+                Matchers.instanceOf(Note::class.java),
+                Matchers.equalTo(testNotes[0])
+            )
+        ).perform(ViewActions.click())
+
+        Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.action_next),
+                ViewMatchers.isEnabled()
+            )
+        ).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.noteTitleText))
+            .check(ViewAssertions.matches(ViewMatchers.withText(testNotes[1].title)))
+        Espresso.onView(ViewMatchers.withId(R.id.noteBodyText))
+            .check(ViewAssertions.matches(ViewMatchers.withText(testNotes[1].text)))
+    }
+
+    @Test
+    fun shouldNavigateToPreviousNote() {
+        Espresso.onData(
+            Matchers.allOf(
+                Matchers.instanceOf(Note::class.java),
+                Matchers.equalTo(testNotes[1])
+            )
+        ).perform(ViewActions.click())
+
+        Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.action_back),
+                ViewMatchers.isEnabled()
+            )
+        ).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.noteTitleText))
+            .check(ViewAssertions.matches(ViewMatchers.withText(testNotes[0].title)))
+        Espresso.onView(ViewMatchers.withId(R.id.noteBodyText))
+            .check(ViewAssertions.matches(ViewMatchers.withText(testNotes[0].text)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.action_back))
+            .check(ViewAssertions.matches(ViewMatchers.isEnabled()))
     }
 }
